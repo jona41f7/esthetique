5c7556a280f6466fe6ceb0b05f71164c11736149
jona41f7 <jona41f7@stud.kea.dk>
2021-03-23 15:33:20 +0100

    aniamtion added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 gsap.registerPlugin(ScrollTrigger);
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+  const divs = section.querySelectorAll("div");
+
+  gsap.from(divs, {
+    opacity: 0,
+    scale: 0.3,
+    duration: 1,
+    scrollTrigger: {
+      trigger: section,
+      start: "top bottom",
+      end: "center center",
+      //   markers: true,
+      scrub: true,
+    },
+  });
+});
+
+gsap.from(".left-1", {
+  x: -300,
+  duration: 1,
+  opacity: 0.2,
+  scrollTrigger: {
+    trigger: ".left-1",
+    start: "top bottom",
+    end: "center center",
+    scrub: true,
+  },
+});
+
+gsap.from(".right-1", {
+  x: 300,
+  duration: 1,
+  opacity: 0.2,
+  scrollTrigger: {
+    trigger: ".right-1",
+    start: "top bottom",
+    end: "center center",
+    scrub: true,
+  },
+});
